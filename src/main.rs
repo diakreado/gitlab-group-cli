7ab463f3e9b4738e0c5a6311df41bef6314ebf81
diakreado <diakreado@gmail.com>
2025-03-22 17:04:23 +0300

    feat: get user cmd

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(name = "GitLab CLI")]
-#[command(version, about = "Command line ineterface for GitLab", long_about = None)]
+#[command(name = "GitLab CLI", version, about = "Command line ineterface for GitLab", long_about = None)]
 struct Cli {
-    #[arg(short, long)]
-    name: String,
-
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[command(subcommand)]
+    command: Commands,
 }
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Get entity
+    Get {
+        #[command(subcommand)]
+        command: GetAction,
+    },
+}
+
+#[derive(Subcommand)]
+enum GetAction {
+    /// User entity
+    #[command(alias="u")]
+    User(GetUserArgs),
+}
+
+#[derive(Args)]
+struct GetUserArgs {
+    username: String,
+}
+
 
 fn main() {
     let cli = Cli::parse();
 
-    for _ in 0..cli.count {
-        println!("Hello {}!", cli.name);
+    match &cli.command {
+        Commands::Get { command } => match command {
+            GetAction::User(username) => {
+                println!("Performing action with option: {}", username.username);
+            },
+        }
     }
 }
